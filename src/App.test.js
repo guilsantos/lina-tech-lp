93d64e04a8140ec93ae663c7b0f2cc6f938d07aa
Guilherme Silva <gsilva@teamworks.com>
2022-10-31 14:18:05 -0300

    Format files

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history'
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import App from "./App";
 
 const history = createMemoryHistory();
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(
     <Router history={history}>
       <App />
